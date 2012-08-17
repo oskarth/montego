5b3213869e2fb270aeeac8c2f733295bba1d59ba
Oskar Thoren <o.thoren@gmail.com>
2012-08-17 17:06:43 -0400

    Format code.

--- montego.go
+++ montego.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"math/rand"
-  "os"
-  "runtime/pprof"
-  "time"
-  "fmt"
-  "flag"
+	"os"
+	"runtime/pprof"
+	"time"
 )
 
 // BENCHMARKS
@@ -25,36 +25,36 @@ func withinCircle(x float64, y float64) bool {
 
 func worker(n int, ch chan float64) {
 	var num int
-  source := rand.NewSource(time.Now().UnixNano())
-  random := rand.New(source)
-  for i := 0; i < n; i++ {
-    if withinCircle(random.Float64(), random.Float64()) {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	for i := 0; i < n; i++ {
+		if withinCircle(random.Float64(), random.Float64()) {
 			num++
 		}
 	}
 
-  ch <- float64(num) / float64(n)
+	ch <- float64(num) / float64(n)
 }
 
 func main() {
-  flag.Parse()
-  if *cpuprofile != "" {
-    f, err := os.Create(*cpuprofile)
-    if err != nil {
-      panic(err)
-    }
-    if err := pprof.StartCPUProfile(f); err != nil {
-      panic(err)
-    }
-    defer pprof.StopCPUProfile()
-  }
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			panic(err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			panic(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
-  ch := make(chan float64, NCPU)
+	ch := make(chan float64, NCPU)
 	n := 10000000 // number of points total
-  nNCPU := n / NCPU
+	nNCPU := n / NCPU
 	var pi float64
 
-  t0 := time.Now()
+	t0 := time.Now()
 	for i := 0; i < NCPU; i++ {
 		go worker(nNCPU, ch)
 	}
@@ -63,8 +63,8 @@ func main() {
 		pi += <-ch
 	}
 
-  pi = (pi / float64(NCPU)) * 4.0
+	pi = (pi / float64(NCPU)) * 4.0
 
-  t1 := time.Now()
+	t1 := time.Now()
 	fmt.Println(pi, "took ", t1.Sub(t0), "to calculate.")
 }
